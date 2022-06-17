71dba44bd5193b93d3eb5d220ec21fa64db1ec5b
Steodec <paul.tedesco@humbrain.com>
2022-06-17 10:48:32 +0200

    Update

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -4,9 +4,8 @@ namespace Steodec\Router\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
-class Route
-{
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Route {
     private ?string $_path;
     private ?string $_callable;
     private ?string $_method;
@@ -14,40 +13,35 @@ class Route
     private ?string $_is_granted;
 
 
-    public function __construct(?string $method = null, string $path = null, string $name = null, string $isGranted = null)
-    {
-        $this->_path = $path;
-        $this->_name = $name;
-        $this->_method = $method;
+    public function __construct(?string $method = NULL, string $path = NULL, string $name = NULL, string $isGranted = NULL) {
+        $this->_path       = $path;
+        $this->_name       = $name;
+        $this->_method     = $method;
         $this->_is_granted = $isGranted;
     }
 
-    public function setCallable(string $string)
-    {
+    public function setCallable(string $string) {
         $this->_callable = $string;
     }
 
     /**
      * @param string|null $name
      */
-    public function setName(?string $name): void
-    {
+    public function setName(?string $name): void {
         $this->_name = $name;
     }
 
     /**
      * @param string|null $method
      */
-    public function setMethod(?string $method): void
-    {
+    public function setMethod(?string $method): void {
         $this->_method = $method;
     }
 
     /**
      * @param string|null $path
      */
-    public function setPath(?string $path): void
-    {
+    public function setPath(?string $path): void {
         $this->_path = $path;
     }
 
@@ -55,42 +49,37 @@ class Route
     /**
      * @return string|null
      */
-    public function getPath(): ?string
-    {
+    public function getPath(): ?string {
         return $this->_path;
     }
 
     /**
      * @return string
      */
-    public function getCallable(): string
-    {
+    public function getCallable(): string {
         return $this->_callable;
     }
 
     /**
      * @return string|null
      */
-    public function getMethod(): ?string
-    {
+    public function getMethod(): ?string {
         return $this->_method;
     }
 
     /**
      * @return string|null
      */
-    public function getName(): ?string
-    {
+    public function getName(): ?string {
         return $this->_name;
     }
 
     /**
      * @return string|null
      */
-    public function getIsGranted(): ?string
-    {
+    public function getIsGranted(): ?string {
         return $this->_is_granted;
     }
-    
+
 
 }
